23075e3b7d60e948ed297232906abc2275ea982d
cangcang <cangcang_code@icloud.com>
2026-08-17 15:41:46 +0800

    openos-run: 移除多余的 #include apps.moc
    qt6.preprocess(moc_headers) 已自动生成并编译 moc_apps.cpp,
    手动 include 反而找不到文件

--- apps.cpp
+++ apps.cpp
@@ -63,5 +63,3 @@ void AppLauncher::launchInVmapp(const QString& vmapp, const QString& exec) {
     Q_UNUSED(vmapp);
     launch(exec);
 }
-
-#include "apps.moc"   // Qt moc: AppsModel/AppLauncher 的 Q_OBJECT vtable
